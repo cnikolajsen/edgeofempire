ca76d4b878c9a36d6be596d7c443177920802573
Christian Nikolajsen <cn@peytz.dk>
2013-03-08 14:55:17 +0100

    Set up frontend user login with devise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,15 @@
 class UsersController < ApplicationController
+  before_filter :authenticate_user!
+
+  def index
+    @users = User.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @users }
+    end
+  end
+    
   def new
     @user = User.new
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130307132700) do
+ActiveRecord::Schema.define(:version => 20130308131426) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -63,22 +63,33 @@ ActiveRecord::Schema.define(:version => 20130307132700) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "username",      :default => "",   :null => false
-    t.string   "email",         :default => "",   :null => false
-    t.string   "password_hash", :default => "",   :null => false
-    t.string   "password_salt", :default => "",   :null => false
-    t.string   "first_name",    :default => "",   :null => false
-    t.string   "last_name",     :default => "",   :null => false
-    t.string   "city",          :default => "",   :null => false
-    t.string   "state",         :default => "",   :null => false
-    t.string   "postal_code",   :default => "",   :null => false
-    t.integer  "country_id",    :default => 0,    :null => false
-    t.boolean  "enabled",       :default => true, :null => false
+    t.string   "username",               :default => "",   :null => false
+    t.string   "email",                  :default => "",   :null => false
+    t.string   "password_hash",          :default => "",   :null => false
+    t.string   "password_salt",          :default => "",   :null => false
+    t.string   "first_name",             :default => "",   :null => false
+    t.string   "last_name",              :default => "",   :null => false
+    t.string   "city",                   :default => "",   :null => false
+    t.string   "state",                  :default => "",   :null => false
+    t.string   "postal_code",            :default => "",   :null => false
+    t.integer  "country_id",             :default => 0,    :null => false
+    t.boolean  "enabled",                :default => true, :null => false
     t.text     "notes"
-    t.datetime "created_at",                      :null => false
-    t.datetime "updated_at",                      :null => false
+    t.datetime "created_at",                               :null => false
+    t.datetime "updated_at",                               :null => false
+    t.string   "encrypted_password",     :default => "",   :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
   end
 
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["enabled"], :name => "index_users_on_enabled"
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
