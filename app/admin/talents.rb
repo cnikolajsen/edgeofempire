d18efa89de3def6534b5187bc262f41e82dc801c
Christian Nikolajsen <cn@peytz.dk>
2013-05-27 13:24:09 +0200

    Replace dice token in admin view of talents

--- app/admin/talents.rb
+++ app/admin/talents.rb
@@ -6,12 +6,22 @@ ActiveAdmin.register Talent do
   index do
     column :name
     column :description do |desc|
+      #text_replace_tokens(desc.description.gsub("\n", "<br />")).html_safe
       truncate(desc.description)
     end
     column :activation
     default_actions
   end
   
+  show do
+    div do
+      strong "Activation: #{talent.activation}"
+    end
+    div do
+      simple_format text_replace_tokens(talent.description)
+    end
+  end
+
   filter :name
 
   controller do
