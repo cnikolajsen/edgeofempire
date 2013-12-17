ec8efd81e0291770b656e7d37d3a57290b036692
Christian Nikolajsen <cn@peytz.dk>
2013-12-17 13:07:03 +0100

    A lot of nice zurb template changes

--- app/helpers/characters_helper.rb
+++ app/helpers/characters_helper.rb
@@ -5,7 +5,7 @@ module CharactersHelper
     if character.creation?
       @return['state_short'] = "Creation"
       @return['state_message'] = "This character is in it creation phase. Special rules may apply."
-      @return['state_label_class'] = "warning"
+      @return['state_label_class'] = "alert"
       @return['state_alert_class'] = "warning"
     elsif character.active?
       @return['state_short'] = "Active"
@@ -15,7 +15,7 @@ module CharactersHelper
     elsif character.retired?
       @return['state_short'] = "Retired"
       @return['state_message'] = "Character taken off duty and is read only."
-      @return['state_label_class'] = "inverse"
+      @return['state_label_class'] = "secondary"
       @return['state_alert_class'] = "info"
     end
 
