c636a9334c881b69ed54ed9aa61528d1b36147a3
Christian Nikolajsen <cn@peytz.dk>
2014-07-08 09:30:55 +0200

    Added batch actions to set attachments on weapons and armor

--- app/admin/armors.rb
+++ app/admin/armors.rb
@@ -55,10 +55,22 @@ ActiveAdmin.register Armor do
     f.actions
   end
 
+  batch_priority = 0
   ArmorCategory.where(:true).each do |i|
-    batch_action "Set Category '#{i.name}' on" do |selection|
+    batch_priority += 1
+    batch_action "Set Category '#{i.name}' on", :priority => batch_priority do |selection|
       Armor.find(selection).each do |armor|
         armor.update_attribute(:armor_category_id, i.id)
+      end
+      redirect_to :back
+    end
+  end
+
+  ArmorAttachment.where(:true).each do |i|
+    batch_priority += 1
+    batch_action "Add attachment '#{i.name}' to", :priority => batch_priority do |selection|
+      Armor.find(selection).each do |armor|
+        ArmorAttachmentsArmor.where(:armor_id => armor.id, :armor_attachment_id => i.id).first_or_create
       end
       redirect_to :back
     end
