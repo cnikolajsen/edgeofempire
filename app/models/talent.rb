549420642b47416a47868cf73907955c70715169
Christian Nikolajsen <cn@peytz.dk>
2014-07-24 14:42:36 +0200

    Frontend editable talents

--- app/models/talent.rb
+++ app/models/talent.rb
@@ -1,9 +1,12 @@
 class Talent < ActiveRecord::Base
   include CharactersHelper
 
-  has_many :character_talents, :dependent => :destroy
+  has_many :character_talents
   has_many :characters, :through => :character_talents
   has_many :races, :through => :race_talents
+
+  validates :name, presence: true, uniqueness: true
+  validates :activation, presence: true
 
   default_scope { order('name ASC') }
 
