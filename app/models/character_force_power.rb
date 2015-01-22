ec4f6716cb89866693ccd18fd671fbcae4926784
Christian Nikolajsen <cn@peytz.dk>
2015-01-22 11:08:13 +0100

    Fixed error when trying to delete character.

--- app/models/character_force_power.rb
+++ app/models/character_force_power.rb
@@ -3,7 +3,7 @@ class CharacterForcePower < ActiveRecord::Base
   belongs_to :force_power
 
   has_many :character_force_power_upgrades, :dependent => :destroy
-  has_many :force_power_upgrades, :through => :character_force_power_upgrades
+  #has_many :force_power_upgrades, :through => :character_force_power_upgrades
   accepts_nested_attributes_for :character_force_power_upgrades, :reject_if => :all_blank, :allow_destroy => true
 
 end
