19905cf0d8cbead664de83691ac7ad1cc3145d94
Christian Nikolajsen <cn@peytz.dk>
2015-01-23 06:24:38 +0100

    Foo'd some tables (FooTable)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,22 +11,21 @@ module ApplicationHelper
   end
 
   def text_replace_tokens(text)
-
-    text = text.gsub(/\[ability\]/, image_tag("dice/ability.png", :alt => "Ability", :class => 'dice', :size => '12'))
-    text = text.gsub(/\[advantage\]/, image_tag("dice/advantage.png", :alt => "Advantage", :class => 'dice', :size => '12'))
-    text = text.gsub(/\[threat\]/, image_tag("dice/threat.png", :alt => "Threat", :class => 'dice', :size => '12'))
-    text = text.gsub(/\[boost\]/, image_tag("dice/boost.png", :alt => "Boost", :class => 'dice', :size => '12'))
-    text = text.gsub(/\[challenge\]/, image_tag("dice/challenge.png", :alt => "Challenge", :class => 'dice', :size => '12'))
-    text = text.gsub(/\[difficulty\]/, image_tag("dice/difficulty.png", :alt => "Difficulty", :class => 'dice', :size => '12'))
-    text = text.gsub(/\[proficiency\]/, image_tag("dice/proficiency.png", :alt => "Proficiency", :class => 'dice', :size => '12'))
-    text = text.gsub(/\[setback\]/, image_tag("dice/setback.png", :alt => "Setback", :class => 'dice', :size => '12'))
-    text = text.gsub(/\[triumph\]/, image_tag("dice/triumph.png", :alt => "Triumph", :class => 'dice', :size => '12'))
-    text = text.gsub(/\[despair\]/, image_tag("dice/despair.png", :alt => "Despair", :class => 'dice', :size => '12'))
-    text = text.gsub(/\[success\]/, image_tag("dice/success.png", :alt => "Success", :class => 'dice', :size => '12'))
-    text = text.gsub(/\[failure\]/, image_tag("dice/failure.png", :alt => "Failure", :class => 'dice', :size => '12'))
-    text = text.gsub(/\[force\]/, image_tag("dice/force.png", :alt => "Force", :class => 'dice', :size => '12'))
-    text = text.gsub(/\[force_white\]/, image_tag("dice/force_white.png", :alt => "Light Side", :class => 'dice', :size => '12'))
-    text = text.gsub(/\[force_black\]/, image_tag("dice/force_black.png", :alt => "Dark side", :class => 'dice', :size => '12'))
+    text = text.gsub(/\[(ability|ab)\]/i, image_tag("dice/ability.png", :alt => "Ability", :class => 'dice', :size => '12'))
+    text = text.gsub(/\[(advantage|ad)\]/i, image_tag("dice/advantage.png", :alt => "Advantage", :class => 'dice', :size => '12'))
+    text = text.gsub(/\[(threat|t)\]/i, image_tag("dice/threat.png", :alt => "Threat", :class => 'dice', :size => '12'))
+    text = text.gsub(/\[(boost|b)\]/i, image_tag("dice/boost.png", :alt => "Boost", :class => 'dice', :size => '12'))
+    text = text.gsub(/\[(challenge|c)\]/i, image_tag("dice/challenge.png", :alt => "Challenge", :class => 'dice', :size => '12'))
+    text = text.gsub(/\[(difficulty|d)\]/i, image_tag("dice/difficulty.png", :alt => "Difficulty", :class => 'dice', :size => '12'))
+    text = text.gsub(/\[(proficiency|p)\]/i, image_tag("dice/proficiency.png", :alt => "Proficiency", :class => 'dice', :size => '12'))
+    text = text.gsub(/\[(setback|s)\]/i, image_tag("dice/setback.png", :alt => "Setback", :class => 'dice', :size => '12'))
+    text = text.gsub(/\[(triumph|tri)\]/i, image_tag("dice/triumph.png", :alt => "Triumph", :class => 'dice', :size => '12'))
+    text = text.gsub(/\[(despair|des)\]/i, image_tag("dice/despair.png", :alt => "Despair", :class => 'dice', :size => '12'))
+    text = text.gsub(/\[(success|su)\]/i, image_tag("dice/success.png", :alt => "Success", :class => 'dice', :size => '12'))
+    text = text.gsub(/\[(failure|f)\]/i, image_tag("dice/failure.png", :alt => "Failure", :class => 'dice', :size => '12'))
+    text = text.gsub(/\[(force|fo)\]/i, image_tag("dice/force.png", :alt => "Force", :class => 'dice', :size => '12'))
+    text = text.gsub(/\[(force_white|fow)\]/i, image_tag("dice/force_white.png", :alt => "Light Side", :class => 'dice', :size => '12'))
+    text = text.gsub(/\[(force_black|fob)\]/i, image_tag("dice/force_black.png", :alt => "Dark side", :class => 'dice', :size => '12'))
 
     text.html_safe
   end
